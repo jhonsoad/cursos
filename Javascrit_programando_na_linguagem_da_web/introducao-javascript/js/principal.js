4fb6b7f683dbe5128a0cfceda927675838edaff2
Jhonatas Magalhaes <jhonatasbarbosa@yahoo.com.br>
2022-10-31 21:55:06 -0300

    javascript-aula 2

--- Javascrit_programando_na_linguagem_da_web/introducao-javascript/js/principal.js
+++ Javascrit_programando_na_linguagem_da_web/introducao-javascript/js/principal.js
@@ -1,5 +1,32 @@
-console.log("Fui carregado de um arquivo externo")
-
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector('.info-altura');
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector('.info-imc')
+
+var pesoEhValido = true;
+var alturaEhvalida = true;
+
+if(peso <= 0 || peso >= 1000){
+    tdImc.textContent = 'Peso inválido';
+    console.log('Peso inválido');
+    pesoEhValido = false;
+}
+
+if(altura <= 0 || altura >= 3.00 ) {
+    tdImc.textContent = 'Altura inválida';
+    console.log('Altura inválida');
+    alturaEhvalida = false
+}
+
+if(pesoEhValido && alturaEhvalida){
+    var imc = peso/(altura*altura);
+    tdImc.textContent = imc;
+}
