d871961ad746cde1166ba95ca64c3dcee05eba7b
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2023-07-31 22:10:48 -0300

    Portifolio HTML e CSS, memoteca aula 1

--- Angular/memoteca/src/app/app-routing.module.ts
+++ Angular/memoteca/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HtmCssComponent } from './HTM-CSS/htm-css/htm-css.component';
+import { AboutComponent } from './HTM-CSS/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HtmCssComponent },
+  { path: 'sobre', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/memoteca/src/app/app.module.ts
+++ Angular/memoteca/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { HtmCssComponent } from './HTM-CSS/htm-css/htm-css.component';
+import { AboutComponent } from './HTM-CSS/about/about.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
-    RodapeComponent
+    RodapeComponent,
+    HtmCssComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([]),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
