e26f5fec9fec309d67263c830f44534e558e1716
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2023-11-01 00:32:24 -0300

    ajustes

--- Angular/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ Angular/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento/pensamento';
 import { PensamentoService } from '../pensamento.service';
 
