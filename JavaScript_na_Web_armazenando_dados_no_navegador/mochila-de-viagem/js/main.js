2658d9122fd48dc0bc31caae8317b99aaab3163f
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2023-11-25 16:21:15 -0300

    js armazenando dados no navegador - aula 1

---
+++ JavaScript_na_Web_armazenando_dados_no_navegador/mochila-de-viagem/js/main.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('novoItem');
+const lista = document.getElementById('lista')
+
+
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    const nome = evento.target['nome'].value;
+    const quantidade = evento.target['quantidade'].value;
+
+    criaElemento(nome, quantidade)
+
+})
+
+function criaElemento(nome, quantidade) {
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add('item')
+    
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+    
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+
+    lista.appendChild(novoItem)
+}
