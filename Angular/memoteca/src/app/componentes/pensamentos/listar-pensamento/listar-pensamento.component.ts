9b350d10f0a05639bbbde49ba783d48ecfef967e
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2024-02-17 20:26:04 -0300

    angular 14 evoluindo a aplicacao - aula 4

--- Angular/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ Angular/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,23 +12,34 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
+  filtro: string = ''
 
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => { // usamos o subscribe para receber o retorno do listar() guardamos a resposta na propriedade listaPensamentos e colocamos em uma variavel global
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => { // usamos o subscribe para receber o retorno do listar() guardamos a resposta na propriedade listaPensamentos e colocamos em uma variavel global
       this.listaPensamentos = listaPensamentos;
     })
   }
 
   carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual) // incrementa +1 na paginaAtual antes de fazer a chamada
+    this.service.listar(++this.paginaAtual, this.filtro) // incrementa +1 na paginaAtual antes de fazer a chamada
       .subscribe((listaPensamentos) => {
         this.listaPensamentos.push(...listaPensamentos); // acresenta os dados da nova pagina ao array que tem os dados da pagina anterior
         if(!listaPensamentos.length) { // listaPensamentos.length retorna o numero de elementos na lista, então o !listaPensamentos verifica que a lsta esta vazia
           this.haMaisPensamentos = false;
         }
       })
+  } 
+
+  pesquisarPensamentos() {
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1;
+    this.listaPensamentos = [];
+    this.service.listar(this.paginaAtual, this.filtro)
+      .subscribe((listaPensamentos) => {
+        this.listaPensamentos = listaPensamentos;
+      });
   }
 
 }
--- Angular/memoteca/src/app/componentes/pensamentos/pensamento.service.ts
+++ Angular/memoteca/src/app/componentes/pensamentos/pensamento.service.ts
@@ -11,13 +11,17 @@ export class PensamentoService {
   private readonly API = 'http://localhost:3000/pensamentos';
   constructor(private http: HttpClient) { } //O servico httpClient é usado para fazer requisições e consumir apis 
 
-  listar(pagina: number): Observable<Pensamento[]> {
-    const intensPorPagina = 6; //GET /post?_page=7&_limit=20 formato para criar paginacao usando o json-server
+  listar(pagina: number, filtro: string): Observable<Pensamento[]> {
+    const intensPorPagina = 6; //GET /post?_page=7&_limit=20 formato para criar paginacao
 
     let params = new HttpParams() //HttpParams é uma forma melhor de passarmos parametros na requisição ao inves de ficarmos concatenando
       .set("_page", pagina) // metodo set serve para substituir um valor, passamos o nome do parametro e valor
       .set("_limit", intensPorPagina);
-    
+
+    if(filtro.trim().length > 2) { //.trim remove os esoaços vazios dessa string
+      params = params.set("q", filtro) // o q é do full-text search usado para fazer pesquisa por string
+    } // O if verifica que se o essa string menos os espcos vazios for maior que 2
+
     //return this.http.get<Pensamento[]>(`${this.API}?page=${pagina}&_limit=${intensPorPagina}`);
     // o metodo http do httpClient é usado para fazer as requisições passando metodo que usaremos nesse aso o get, podemos tipar o retorno passando o diamnante "<>" e entre parenteses a url da api
     
