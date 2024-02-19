16161ca75aa62b92d443fb082c71ea5f1986223d
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2024-02-18 21:09:12 -0300

    angular 14 evoluindo a aplicacao - aula 5

--- Angular/memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ Angular/memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,30 +1,41 @@
 
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Pensamento } from './pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-pensamento',
   templateUrl: './pensamento.component.html',
   styleUrls: ['./pensamento.component.css']
 })
-export class PensamentoComponent implements OnInit {
+export class PensamentoComponent {
 
   @Input()  pensamento: Pensamento = {
     id: 0,
     conteudo: 'I love Angular',
     autoria: 'Jhon',
-    modelo: 'modelo3'
+    modelo: 'modelo3',
+    favorito: false
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(private service: PensamentoService) { }
 
   larguraPensamento(): string {
     if (this.pensamento.conteudo.length >= 256) {
       return 'pensamento-g'
     }
     return 'pensamento-p'
+  }
+
+  mudarIconeFavorito() {
+    if (this.pensamento.favorito == false) {
+      return 'inativo'
+    }
+    return 'ativo'
+  }
+
+  atualizarFavoritos() {
+    this.service.mudarFavorito(this.pensamento).subscribe()
+    console.log("Favorito: ", this.pensamento.favorito)
   }
 }
