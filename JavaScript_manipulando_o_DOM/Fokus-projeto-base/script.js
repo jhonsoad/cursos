f0279bdf9e81c9bbfa37a3b02df30ba0e1daf330
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2025-04-05 19:49:17 -0300

    JS manipulando eventos no DOM - aula 4

--- JavaScript_manipulando_o_DOM/Fokus-projeto-base/script.js
+++ JavaScript_manipulando_o_DOM/Fokus-projeto-base/script.js
@@ -1,17 +1,20 @@
 const html = document.querySelector('html')
-const displayTempo = document.querySelector('#timer')
-const titulo = document.querySelector('.app__title')
-const botaoPlay = document.querySelector('.app__card-primary-button')
-const botoes = document.querySelectorAll('.app__card-button')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
-const duracaoFoco = 1500
-const duracaoDescansoCurto = 300
-const duracaoDescansoLongo = 900
+const audioTempoFinalizado = new Audio('/sons/beep.mp3')
+const audioPlay = new Audio('/sons/play.wav')
+const audioPause = new Audio('/sons/pause.mp3')
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
+
 musica.loop = true
 
 musicaFocoInput.addEventListener('change', () => {
@@ -21,7 +24,6 @@ musicaFocoInput.addEventListener('change', () => {
         musica.pause()
     }
 })
-
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
@@ -66,4 +68,32 @@ function alterarContexto(contexto) {
         default:
             break
     }
+}
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        audioTempoFinalizado.play()
+        zerar()
+        alert('Tempo finalizado!')
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        audioPause.play()
+        zerar()
+        return
+    }
+    audioPlay.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
 }
