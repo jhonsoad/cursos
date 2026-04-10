2b3a675d574d9c1433b01bf2d09263843c95f705
Jhon <jhonatasbarbosa@yahoo.com.br>
2026-04-09 21:58:42 -0300

    refactor: remove projetos que não usam angular

--- Java/PrimeiroPrograma/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        float nota = 8.5f;
-        System.out.println("Olá, Mundo!");
-        System.out.println("Sua nota é " + nota);
-    }
-}
--- Java/primeiroprojeto/src/Perfil.java
+++
@@ -1,6 +0,0 @@
-public class Perfil {
-    public static void main(String[] args) {
-        System.out.println("Olá, Jhonatas");
-        System.out.println("tudo bem?");
-    }
-}
