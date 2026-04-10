fbe35284dd5c4bd0f5c6fb2ac792e5972f740036
Jhon <jhonatasbarbosa@yahoo.com.br>
2026-04-09 22:07:00 -0300

    refactor: remove projetos que não são typescript

--- Angular/cursos-js/src/app/components/cursos/js-armazenando-dados-no-navegador/item.ts
+++
@@ -1,5 +0,0 @@
-export interface Item {
-    id: number,
-    nome: string,
-    quantidade: number,
-}
--- Flutter/image_gallery_android_app/android/app/src/main/kotlin/com/example/image_gallery_android_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.image_gallery_android_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
