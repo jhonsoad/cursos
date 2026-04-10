cd3d2c74727049eb43352cd5eddf5335ae08dd34
Jhon <jhonatasbarbosa@yahoo.com.br>
2026-04-09 22:04:51 -0300

    refactor: remove projetos que não são frontend

--- Flutter/image_gallery_android_app/lib/routes.dart
+++
@@ -1,6 +0,0 @@
-class Routes {
-  static const String register = '/register';
-  static const String imageView = '/image-view';
-  static const String login = '/login';
-  static const String imageGallery = '/image-gallery';
-}
