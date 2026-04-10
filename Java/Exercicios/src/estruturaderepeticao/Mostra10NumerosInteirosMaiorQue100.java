bcd2c1a9212fdbd1f26576a544feddb862dec263
Jhon <jhonatasbarbosa@yahoo.com.br>
2026-04-09 22:12:28 -0300

    refactor: remove projetos que não são next js

--- Java/Exercicios/src/estruturaderepeticao/Mostra10NumerosInteirosMaiorQue100.java
+++
@@ -1,9 +0,0 @@
-package estruturaderepeticao;
-
-public class Mostra10NumerosInteirosMaiorQue100 {
-    public static void main(String[] args) {
-        for (int i = 100; i <=110; i++) {
-            System.out.println(i);
-        }
-    }
-}
--- Jest/curso_jest/modulo II/Aula 3/Product.js
+++
@@ -1,4 +0,0 @@
-module.exports = class Product {
-  description;
-  price;
-};
